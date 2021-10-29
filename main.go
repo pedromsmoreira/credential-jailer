ca146d7c24a7ed97fb49f58f27149f51eb0e6788
pedromsmoreira <pedromsmoreira51@gmail.com>
2021-10-29 15:03:02 +0100

    feat: setup server

--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
-	// endpoint to get credentials
-	// should have authentication throught api key
-	// read from env vars
-	fmt.Println("hello world")
+	reader := &EnvVarReader{
+		proxy: &OsProxy{},
+	}
+
+	server := NewServer(reader)
+	err := server.Start("4000")
+
+	if err != nil {
+		fmt.Printf("Error starting the server. Error: %v", err)
+		os.Exit(1)
+	}
+	ss := make(chan os.Signal, 1)
+	fmt.Println("Sidelogger is running. Ctrl-C to exit")
+	signal.Notify(ss, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-ss
+	fmt.Println("Sidelogger is terminating!")
+	err = server.Shutdown()
+	if err != nil {
+		fmt.Printf("Failed to shutdown API server: %v", err)
+	}
+	fmt.Println("Sidelogger terminated!")
+
+	os.Exit(0)
 }
